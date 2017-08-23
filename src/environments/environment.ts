b274e9d60963f9f532190ad2f40d955ce9658214
Alan Rawkins <arawkins@gmail.com>
2017-08-23 09:35:24 -0700

    added firebase dependency, added firebase to environment config

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCc6bFLQcewMwb0H5JHpnUIki1CvfSwdwA",
+        authDomain: "ionic-todos-3756f.firebaseapp.com",
+        databaseURL: "https://ionic-todos-3756f.firebaseio.com",
+        projectId: "ionic-todos-3756f",
+        storageBucket: "",
+        messagingSenderId: "785333069311"
+    }
+};
